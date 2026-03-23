14af8f3bd9da49cc662e520931b3b8b6751a5992
Sarthak <sarthaksaini208@gmail.com>
2026-03-24 00:38:43 +0530

    Leetcode Questions

---
+++ 1539. Kth Missing Positive Number/1539. Kth Missing Positive Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] - mid - 1 < k)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return start + k;
+    }
+}
